8d05f2ffe64d6ab7d7005b8bd3969c7a1da2c76e
jinushah <jinaygshah10@gmail.com>
2022-10-15 09:27:25 +0530

    completed

--- 2/3.py
+++ 2/3.py
@@ -1,12 +1,18 @@
 import os
+from tokenize import String
 os.system('cls')
 
-str1="Lorem ipsum dolor sit amet, consectetur adip"
+lose=[]
+string = ""
+num1=int(input("How Many Elements You Want to Add:"))
 
-# for i in range(0,num1):
-#     board=input("Enter String:")
-#     lose.append(board)
+for i in range(0,num1):
+   num=input("Enter String:")
+   string += num
+   
+# print(string)
+lose = list(string)
 
-lst=list(filter(lambda x:True if x.lower() in "aeiou" else False, str1))
+lst=list(filter(lambda x:True if x.lower() in "aeiou" else False, lose))
 
 print(lst)
